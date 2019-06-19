1f8d2cbe9b363208fa022bae2ee59f1ff031cd8c
David McClure <davidwilliammcclure@gmail.com>
2019-06-18 21:24:43 -0400

    Upgrade to terraform 0.12 syntax.

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "aws_region" {
-  value = "${var.aws_region}"
+  value = var.aws_region
 }
 
 output "vpc_id" {
-  value = "${aws_vpc.vpc.id}"
+  value = aws_vpc.vpc.id
 }
 
 output "subnet_id" {
-  value = "${aws_subnet.vpc.id}"
+  value = aws_subnet.vpc.id
 }
